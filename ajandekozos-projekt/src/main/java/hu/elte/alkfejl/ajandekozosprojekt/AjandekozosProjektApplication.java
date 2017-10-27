a65fc4f8fc44138eda72b721da239b640055082b
Negrut Ákos <sokaturgen-17@hotmail.com>
2017-10-27 22:11:51 +0200

    Legenerált kiinduló SB projekt

---
+++ ajandekozos-projekt/src/main/java/hu/elte/alkfejl/ajandekozosprojekt/AjandekozosProjektApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.ajandekozosprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjandekozosProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjandekozosProjektApplication.class, args);
+	}
+}
