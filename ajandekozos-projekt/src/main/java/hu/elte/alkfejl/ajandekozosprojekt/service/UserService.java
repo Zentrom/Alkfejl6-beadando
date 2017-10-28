9862259cf37e5eae638cc1bd3f78fa388b9fe934
Negrut Ákos <sokaturgen-17@hotmail.com>
2017-10-28 19:06:13 +0200

    Kezdetleges Authorizáció és REST API

--- ajandekozos-projekt/src/main/java/hu/elte/alkfejl/ajandekozosprojekt/service/UserService.java
+++ ajandekozos-projekt/src/main/java/hu/elte/alkfejl/ajandekozosprojekt/service/UserService.java
@@ -1,20 +1,42 @@
 package hu.elte.alkfejl.ajandekozosprojekt.service;
 
 import hu.elte.alkfejl.ajandekozosprojekt.model.User;
+import static hu.elte.alkfejl.ajandekozosprojekt.model.User.Role.USER;
 import hu.elte.alkfejl.ajandekozosprojekt.repository.UserRepository;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Service
+@SessionScope
+@Data
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void register(User user) {
-        userRepository.save(user);
+    private User user;
+
+    public User login(User user) //throws UserNotValidException 
+    {
+        if (isValid(user)) {
+            return this.user = userRepository.findByUsername(user.getUsername()).get();
+        }
+        //throw new UserNotValidException();
+        return null;
+    }
+
+    public User register(User user) {
+        user.setRole(USER);
+        this.user = userRepository.save(user);
+        return user;
     }
 
     public boolean isValid(User user) {
         return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()).isPresent();
+    }
+
+    public boolean isLoggedIn() {
+        return user != null;
     }
 }
